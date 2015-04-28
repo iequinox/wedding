aaca823a9e0e6eb6af360e4c624d73ce4f50fa4d
Ye Wang <ye.wang@hulu.com>
2015-04-28 23:04:30 +0800

    1. Add robots.txt 2. Change tucao to img title 3. Fix large pic modal vertical center alignment

--- myapp.rb
+++ myapp.rb
@@ -4,9 +4,10 @@ require 'sinatra'
 set :port, 8080
 set :bind, '0.0.0.0'
 
+tucao_arr = File.open('./config/tucao.txt') do |f|
+  f.readlines.map {|l| l.gsub(/^-$/,'').strip}
+end
+
 get '/wedding' do
-  tucao_arr = File.open('./config/tucao.txt') do |f|
-    f.readlines.map {|l| l.gsub(/^-$/,'').strip}
-  end
   erb :wedding, {}, {'tucao_arr' => tucao_arr}
 end
