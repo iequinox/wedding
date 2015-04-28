9b5341247d5727732dd05be6b98c2bcd20741ad5
Ye Wang <ye.wang@hulu.com>
2015-04-29 00:37:37 +0800

    Redirect / to wedding

--- myapp.rb
+++ myapp.rb
@@ -1,10 +1,21 @@
 require 'sinatra'
+#require "rack/cache"
+
+use Rack::Cache
 
 set :port, 8080
 set :bind, '0.0.0.0'
 
 tucao_arr = File.open('./config/tucao.txt') do |f|
   f.readlines.map {|l| l.gsub(/^-$/,'').strip}
+end
+
+#before '/images/*' do
+#  cache_control :public, :max_age => 36000
+#end
+
+get '/' do
+  redirect to('/wedding')
 end
 
 get '/wedding' do
